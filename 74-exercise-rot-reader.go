bc03feea502cc408567b5a01402cebee5413c015
sotetsuk <koyamada-s@sys.i.kyoto-u.ac.jp>
2016-03-27 19:36:40 +0900

    fix typo

--- 74-exercise-rot-reader.go
+++ 74-exercise-rot-reader.go
@@ -23,7 +23,7 @@ func (rot *rot13Reader) Read(b []byte) (n int, err error){
 }
 
 func main() {
-	s := strings.NewReader("Lbh penpxrq gur pdqr!")
+	s := strings.NewReader("Lbh penpxrq gur pbqr!")
 	r := rot13Reader{s}
 	io.Copy(os.Stdout, &r)
 }
